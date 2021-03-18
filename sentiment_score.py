3b729fb573ed8b44113db574e169c0ea5cac506c
borgr <ktilana@gmail.com>
2021-03-18 17:15:27 +0200

    debugged sentiment

--- sentiment_score.py
+++ sentiment_score.py
@@ -19,8 +19,38 @@ def read_lexicon(path):
 
 
 class Scorer:
+    lang_abbr = {'hu': 'hungarian',
+                 'sw': 'swedish',
+                 'ka': 'kazakh',
+                 'no': 'norwegian',
+                 'fi': 'finnish',
+                 'ar': 'arabic',
+                 'in': 'indonesian',
+                 'po': 'portuguese',
+                 'tu': 'turkish',
+                 'az': 'azerbaijani',
+                 'sl': 'slovene',
+                 'sp': 'spanish',
+                 'da': 'danish',
+                 'ne': 'nepali',
+                 'ro': 'romanian',
+                 'gr': 'greek',
+                 'du': 'dutch',
+                 'ta': 'tajik',
+                 'ge': 'german',
+                 'en': 'english',
+                 'ru': 'russian',
+                 'fr': 'french',
+                 'it': 'italian'}
+    nltk_langs = [val for val in lang_abbr.values()]
+
     def __init__(self, lang, lexicon_path):
-        self.stop_words = set(stopwords.words(lang))
+        if len(lang) == 2:
+            lang = Scorer.lang_abbr[lang]
+        if lang in Scorer.nltk_langs:
+            self.stop_words = set(stopwords.words(lang))
+        else:
+            self.stop_words = {}
         self.lexicon = read_lexicon(lexicon_path)
 
     def score_text(self, txt: str):
@@ -47,27 +77,35 @@ class SentimentScorer(Scorer):
         txt = self.remove_stopwords(txt)
         pos = 0
         neg = 0
-        inverse = [1, 1]
+        inverse = [1]
         for word in txt:
-            if inverse:
-                inverse.pop()
             if word not in self.lexicon:
                 continue
             score = self.lexicon[word]
+            print(f"word {word} score {score}")
             if word in self.negative_words:
+                print(f"negative {word}")
                 inverse.append(-1)
+                continue
             else:
                 inverse.append(1)
-            if score * reduce(lambda x, y: x * y, inverse) > 0:
+            # inverse score if needed
+            inverse = inverse[-self.negative_effect_window - 1:]
+            score = score * reduce(lambda x, y: x * y, inverse)
+            print(f"inverse {inverse} updated score {score}")
+            if score > 0:
                 pos += 1
-            else:
+            elif score < 0:
                 neg += 1
+        if neg == pos == 0:
+            return 0
         return (pos - neg) / (pos + neg)
 
 
 if __name__ == '__main__':
     example_sent = """This is a sample sentence, 
-                      showing off the stop words filtration."""
+                      showing off the stop great words filtration."""
 
     scorer = SentimentScorer("en", SENTIMENT_PATH)
-    scorer.score_text(example_sent)
+    score = scorer.score_text(example_sent)
+    print(f"score={score}")
